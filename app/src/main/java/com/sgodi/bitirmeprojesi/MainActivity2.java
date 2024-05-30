aa6ca779d2c2fb2f60e2270393dba48706c218ef
semihgul5 <semih.gul099@gmail.com>
2024-05-30 15:42:33 +0300

    updates

--- app/src/main/java/com/sgodi/bitirmeprojesi/MainActivity2.java
+++ app/src/main/java/com/sgodi/bitirmeprojesi/MainActivity2.java
@@ -1,8 +1,10 @@
 package com.sgodi.bitirmeprojesi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -10,15 +12,17 @@ import androidx.navigation.ui.NavigationUI;
 import android.annotation.SuppressLint;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sgodi.bitirmeprojesi.databinding.ActivityMain2Binding;
 import com.sgodi.bitirmeprojesi.ui.fragments.SahiplenFragment;
 
 public class MainActivity2 extends AppCompatActivity {
     private ActivityMain2Binding binding;
     FragmentManager fragmentManager;
-
+    private NavController navController;
 
     @SuppressLint("ResourceAsColor")
     @Override
@@ -29,8 +33,25 @@ public class MainActivity2 extends AppCompatActivity {
         fragmentManager =getSupportFragmentManager();
 
 
-        NavHostFragment navHostFragment=(NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
-        NavigationUI.setupWithNavController(binding.bottomNavigationView,navHostFragment.getNavController());
+        /*NavHostFragment navHostFragment=(NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navHostFragment.getNavController());*/
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.anaSayfaFragment){
+                    navController.navigate(R.id.anaSayfaFragment);
+                } else if (item.getItemId()==R.id.sahiplenFragment) {
+                    navController.navigate(R.id.sahiplenFragment);
+                } else if (item.getItemId()==R.id.profilGorunumFragment) {
+                    navController.navigate(R.id.profilGorunumFragment);
+                }
+                return false;
+            }
+        });
 
 
         
